a0cde507872295105fa74b1004e4e92b635e6c85
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2023-03-03 21:17:47 +0100

    Testing SCP

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 from paramiko import SSHClient
 from scp import SCPClient
+import sys
 
 from private_file import *
 
@@ -15,11 +16,18 @@ target = TARGET
 # SSH/SCP Directory Recursively
 # def ssh_scp_files(ssh_host, ssh_user, ssh_password, ssh_port, source_volume, destination_volume):
 
+
+# Define progress callback that prints the current percentage completed for the file
+def progress(filename, size, sent):
+    progress = float(sent) / float(size) * 100
+    print(f'{filename} progress: {progress:.2f}%')
+
+
 ssh = SSHClient()
 ssh.load_system_host_keys()
 ssh.connect(hostname=HOST, username=USER, key_filename=PKEY, passphrase=PASSPHRASE)
 
-with SCPClient(ssh.get_transport()) as scp:
+with SCPClient(transport=ssh.get_transport(), progress=progress) as scp:
     scp.get(remote_path=source, local_path=target)
 
 
