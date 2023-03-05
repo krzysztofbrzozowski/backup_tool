6488c809b567cdf24862504319f53127be1f53f4
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2023-03-05 14:39:56 +0100

    Added config manager

--- config_manager.py
+++ config_manager.py
@@ -4,6 +4,7 @@
 @time:      05/03/2023
 @desc:      Deliver paths per setting written in config
 """
+import logging
 import platform
 import yaml
 import os
@@ -11,10 +12,11 @@ import os
 
 class ConfigManager:
     self_name = platform.node()
+    # Change this line if needed
     config_path = os.path.join(os.getenv('BACKUP_TOOL_DIR', None), 'config', 'config_backup_tool_private.yaml')
 
     @classmethod
-    def get_config(cls, test_mode=False):
+    def get_config(cls, test_mode: bool = False):
         with open(cls.config_path, 'r') as file:
             config = yaml.safe_load(file)
 
@@ -22,8 +24,15 @@ class ConfigManager:
             return config['TEST_USER']
         return config[cls.self_name]
 
+    @classmethod
+    def get_config_value(cls, config_key: str = None):
+        try:
+            return cls.get_config()[config_key]
+        except KeyError as e:
+            logging.error(e)
+
 
 if __name__ == '__main__':
-    ConfigManager.get_config()
+    ConfigManager.get_config_value('USER')
     pass
 
