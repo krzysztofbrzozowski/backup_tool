80e8196899ac1f60f2fe6134f70c2f2d1e47d6be
Krzysztof Brzozowski <krzysztof_brzozowski@icloud.com>
2023-04-10 14:45:32 +0200

    Testing login via ssh in CI

--- .github/workflows/prepare_server_test_files.sh
+++ .github/workflows/prepare_server_test_files.sh
@@ -8,4 +8,5 @@ echo "-----BEGIN OPENSSH PRIVATE KEY-----" > id_ed25519_docker
 echo "$1" >> id_ed25519_docker
 echo "-----END OPENSSH PRIVATE KEY-----" >> id_ed25519_docker
 
+realpath id_ed25519_docker
 cat id_ed25519_docker
